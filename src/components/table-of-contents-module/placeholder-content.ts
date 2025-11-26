30986b6e9c37ad54685a7909bf4957cf28b59b5c
burntarrow <65312004+burntarrow@users.noreply.github.com>
2025-11-26 14:53:01 -0600

    Delete src/components/table-of-contents-module/placeholder-content.ts

--- src/components/table-of-contents-module/placeholder-content.ts
+++
@@ -1,9 +0,0 @@
-// src/components/table-of-contents-module/placeholder-content.tsx
-import * as React from 'react';
-
-export const PlaceholderContent: React.FC = () => (
-  <div className="divi-toc-placeholder">
-    <strong>Divi TOC</strong>
-    <p>Headings found in the page will appear here.</p>
-  </div>
-);
