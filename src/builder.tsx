8a674ba6851371d8c05520a931885802bbe6439d
burntarrow <65312004+burntarrow@users.noreply.github.com>
2025-11-27 14:28:01 -0600

    Log when Divi TOC builder bundle is loaded
    Added console log for builder bundle loading.

--- src/builder.tsx
+++ src/builder.tsx
@@ -2,6 +2,11 @@
  * Builder entry: registers the Divi TOC module inside the Divi 5 visual builder.
  * The module definition mirrors the structure used in the Divi 5 example modules.
  */
+
+// top of src/builder.tsx
+// eslint-disable-next-line no-console
+console.log('[Divi TOC] builder bundle loaded');
+
 import { registerModule } from './divi-module-shim';
 import { TableOfContentsModule } from './components/table-of-contents-module';
 import './components/table-of-contents-module/style.scss';
